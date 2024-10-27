d0c6c9199536ac44b3e1a502baf24c08a136ea57
Serhii Samoliuk <samolyk2005@gmail.com>
2024-10-27 14:11:03 +0100

    title component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import { Container, Title } from "@/components/shared";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
    <>
-      <h1>Main</h1>
+      <Container className="mt-10">
+         <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
    </>
   );
 }
