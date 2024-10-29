4a60f5d6c2580e5c4c9792459a5415f40e4c498a
Serhii Samoliuk <samolyk2005@gmail.com>
2024-10-29 10:15:06 +0100

    filters component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,29 @@
-import { Container, Title, TopBar } from "@/components/shared";
+import { Container, Filters, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return (
    <>
       <Container className="mt-10">
          <Title text="All pizzas" size="lg" className="font-extrabold" />
+      </Container>
 
-         <TopBar />
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+         <div className="flex gap-[60px]">
+
+            {/* Filter */}
+            <div className="w-[250px]">
+               <Filters />
+            </div>
+
+            {/* ProductList */}
+            <div className="flex-1">
+               <div className="flex flex-col gap-16">
+                  Product List
+               </div>
+            </div>
+         </div>
       </Container>
    </>
   );
---
+++ src/components/shared/filters.tsx
@@ -0,0 +1,115 @@
+import React from 'react'
+import { Title } from './title';
+import { FilterCheckbox } from './filter-checkbox';
+import { Input } from '../ui';
+import { RangeSlider } from '../ui/range-slider';
+import { CheckboxFiltersGroup } from './checkbox-filters-group';
+
+interface Props {
+   className?: string
+}
+
+export const Filters: React.FC<Props> = ({ className }) => {
+   return (
+      <div className={className}>
+         <Title text='Filter' size='sm' className='mb-5 font-bold'/>
+
+         <div className="flex flex-col gap-4" >
+            <FilterCheckbox text="Can be created" value="1"/>
+            <FilterCheckbox text="New" value="2"/>
+         </div>
+
+         <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+            <p className="font-bold mb-3">Price from & to:</p>
+            <div className="flex gap-3 mb-5">
+               <Input type="number" placeholder="0" min={0} max={500} defaultValue={0} />
+               <Input type="number" min={100} max={500} placeholder="500" />
+            </div>
+
+            <RangeSlider min={0} max={500} step={10} value={[0, 500]} />
+         </div>
+
+         <CheckboxFiltersGroup 
+            title='Ingredients'
+            className='mt-5'
+            limit={6}
+            defaultItems={[
+               {
+                  text: 'Cheese sauce',
+                  value: '1',
+                },
+                {
+                  text: 'Mozzarella',
+                  value: '2',
+                },
+                {
+                  text: 'Garlic',
+                  value: '3',
+                },
+                {
+                  text: 'Pickles',
+                  value: '4',
+                },
+                {
+                  text: 'Red onion',
+                  value: '5',
+                },
+                {
+                  text: 'Tomatoes',
+                  value: '6',
+                },
+            ]}
+            items={[
+               {
+                  text: 'Cheese sauce',
+                  value: '1',
+                },
+                {
+                  text: 'Mozzarella',
+                  value: '2',
+                },
+                {
+                  text: 'Garlic',
+                  value: '3',
+                },
+                {
+                  text: 'Pickles',
+                  value: '4',
+                },
+                {
+                  text: 'Red onion',
+                  value: '5',
+                },
+                {
+                  text: 'Tomatoes',
+                  value: '6',
+                },
+                {
+                  text: 'Cheese sauce',
+                  value: '1',
+                },
+                {
+                  text: 'Mozzarella',
+                  value: '2',
+                },
+                {
+                  text: 'Garlic',
+                  value: '3',
+                },
+                {
+                  text: 'Pickles',
+                  value: '4',
+                },
+                {
+                  text: 'Red onion',
+                  value: '5',
+                },
+                {
+                  text: 'Tomatoes',
+                  value: '6',
+                },
+            ]}
+         />
+      </div>
+   );
+}
