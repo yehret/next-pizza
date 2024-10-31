c4ef43bb0f34debc55a689aacc7f49e09294649c
Serhii Samoliuk <samolyk2005@gmail.com>
2024-10-31 09:19:22 +0100

    product card component

---
+++ src/components/shared/product-card.tsx
@@ -0,0 +1,44 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import React from 'react'
+import { Title } from './title';
+import { Button } from '../ui';
+import { Plus } from 'lucide-react';
+
+interface Props {
+   id: number;
+   name: string;
+   price: number;
+   count?: number;
+   imageUrl: string;
+   className?: string;
+ }
+
+ export const ProductCard: React.FC<Props> = ({ id, name, price, count, imageUrl, className }) => {
+   return (
+      <div className={cn(className)}>
+         <Link href={`/product/${id}`}>
+            <div className="flex justify-center p-6 bg-secondary rounded-lg h-[260px]">
+               <img className="w-[215px] h-[215px]" src={imageUrl} alt={name} />
+            </div>
+
+            <Title text={name} size="sm" className="mb-1 mt-3 font-bold" />
+
+            <p className="text-sm text-gray-400">
+               Chicken, mozarella, cheddar & parmezan, cheese sauce, tomatoes, alfredo sauce, garlic
+            </p>
+
+            <div className="flex justify-between items-center mt-4">
+               <span className="text-[20px]">
+                  from <b>{price} $</b>
+               </span>
+
+               <Button variant="secondary">
+                  <Plus size={20} className="mr-1" />
+                  Добавить
+               </Button>
+            </div>
+         </Link>
+      </div>
+   )
+}
