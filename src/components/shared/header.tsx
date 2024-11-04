ce542c9561107ecc51ed1a95e6f170ca54439bdb
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-04 20:01:22 +0100

    search input layout

--- src/components/shared/header.tsx
+++ src/components/shared/header.tsx
@@ -4,6 +4,8 @@ import { Container } from './container'
 import Image from 'next/image'
 import { Button } from '../ui'
 import { ArrowRight, ShoppingCart, User } from 'lucide-react'
+import Link from 'next/link'
+import { SearchInput } from './search-input'
 
 interface Props {
    className?: string
@@ -14,16 +16,20 @@ export const Header: React.FC<Props> = ({ className }) => {
       <header className={cn('border border-b', className)}>
          <Container className='flex items-center justify-between py-8'>
             {/* Left side */}
-            <div className='flex items-center gap-4'>
-               <Image src={'/logo.png'} alt='Logo' width={35} height={35}/>
-               <div>
-                  <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
-                  <p className='text-sm text-gray-400 leading-3'>nowhere is tastier</p>
+            <Link href={'/'}>
+               <div className='flex items-center gap-4'>
+                  <Image src={'/logo.png'} alt='Logo' width={35} height={35}/>
+                  <div>
+                     <h1 className='text-2xl uppercase font-black'>Next Pizza</h1>
+                     <p className='text-sm text-gray-400 leading-3'>nowhere is tastier</p>
+                  </div>
                </div>
+            </Link>
+
+            {/* Center Side // Search */}
+            <div className='mx-10 flex-1'>
+               <SearchInput />
             </div>
-
-            {/* Center Side */}
-
 
             {/* Right Side */}
             <div className="flex items-center gap-3">
