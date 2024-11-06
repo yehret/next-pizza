2a29b12e5567354875fdb49621db887e8c88c566
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-06 20:11:29 +0100

    products get request

--- services/products.ts
+++ services/products.ts
@@ -1,3 +1,6 @@
+import { Product } from "@prisma/client"
+import { axiosInstance } from "./instance"
+
 export const search = async (query: string) => {
-   
+   return (await axiosInstance.get<Product>('/products/search', { params: { query } })).data
 }
