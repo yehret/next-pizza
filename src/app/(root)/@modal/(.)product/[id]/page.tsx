4415c70640c2f30dfbbf95ab847c975366e5a5f0
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-11 15:36:08 +0100

    modal route setup

---
+++ src/app/(root)/@modal/(.)product/[id]/page.tsx
@@ -0,0 +1,11 @@
+import { Container } from "@/components/shared";
+
+
+export default async function ProductModalPage() {
+
+   return (
+      <Container className="flex flex-col my-10">
+         Modal?
+      </Container>
+   )
+}
