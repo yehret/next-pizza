c0ccb1e0b9cc507ec28fe412757e90c908781e4c
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-22 16:13:51 +0100

    count button, cart item details changes

--- src/shared/components/shared/cart-item-details/cart-item-info.tsx
+++ src/shared/components/shared/cart-item-details/cart-item-info.tsx
@@ -1,18 +1,31 @@
-import { cn } from '@/shared/lib/utils';
+import { mapPizzaType, PizzaSize, PizzaType } from '@/shared/constants/pizza';
+import { Ingredient } from '@prisma/client';
 
 interface Props {
   name: string;
-  details: string;
-  className?: string;
+  pizzaSize?: PizzaSize;
+  type: PizzaType;
+  ingredients: Ingredient[];
 }
 
-export const CartItemInfo: React.FC<Props> = ({ name, details, className }) => {
+export const CartItemInfo: React.FC<Props> = ({ name, pizzaSize, type, ingredients }) => {
+   const details = [];
+
+   if (pizzaSize && type) {
+      const typeName = mapPizzaType[type];
+      details.push(`${typeName} ${pizzaSize} sm`);
+   }
+
+   if(ingredients) {
+      details.push(...ingredients.map((ingredient) => ingredient.name))
+   }
+
   return (
     <div>
-      <div className={cn('flex items-center justify-between', className)}>
+      <div className='flex items-center justify-between'>
         <h2 className="text-lg font-bold flex-1 leading-6">{name}</h2>
       </div>
-      {details && <p className="text-xs text-gray-400 w-[90%]">{details}</p>}
+      {details.length > 0 && <p className="text-xs text-gray-400 w-[90%]">{details.join(', ')}</p>}
     </div>
   );
 };
