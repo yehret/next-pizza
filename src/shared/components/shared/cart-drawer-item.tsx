965f45cb66487be99c3d8adc5e7241e8023dc505
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-22 16:01:50 +0100

    cart item details

---
+++ src/shared/components/shared/cart-drawer-item.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {
+   className?: string
+}
+
+export const CartDrawerItem: React.FC<Props> = ({ className }) => {
+   return (
+      <div className={className}></div>
+   )
+}
--- src/shared/components/shared/cart-drawer.tsx
+++ src/shared/components/shared/cart-drawer.tsx
@@ -34,22 +34,20 @@ export const CartDrawer: React.FC<React.PropsWithChildren<Props>> = ({ children,
 
             <SheetFooter className='-mx-6 mg-white p-8'>
                <div className='w-full'>
-
                   <div className="flex mb-4">
                      <span className="flex flex-1 text-lg text-neutral-500">
                         Total
                         <div className="flex-1 border-b border-dashed border-b-neutral-200 relative -top-1 mx-2" />
                      </span>
-
                      <span className="font-bold text-lg">123 $</span>
                   </div>
 
-               <Link href='/cart'>
-                  <Button type='submit' className='w-full h-12 text-base'>
-                     Make order
-                     <ArrowRight className='w-5 ml-2'/>
-                  </Button>
-               </Link>
+                  <Link href='/cart'>
+                     <Button type='submit' className='w-full h-12 text-base'>
+                        Make order
+                        <ArrowRight className='w-5 ml-2'/>
+                     </Button>
+                  </Link>
                </div>
             </SheetFooter>
          </SheetContent>
