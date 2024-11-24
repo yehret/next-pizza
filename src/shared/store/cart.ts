96e7567a33a6cdea92f69a74c02356f72d66a876
Serhii Samoliuk <samolyk2005@gmail.com>
2024-11-24 15:13:20 +0100

    cart store, fetch cart data

---
+++ src/shared/store/cart.ts
@@ -0,0 +1,42 @@
+import { create } from "zustand";
+import { Api } from "../services/api-client";
+import { getCartDetails } from "../lib";
+import { CartStateItem } from "../lib/get-cart-details";
+
+export interface CartState {
+   loading: boolean,
+   error: boolean,
+   totalAmount: number,
+   items: CartStateItem[],
+
+   fetchCartItems: () => Promise<void>
+   updateItemQuantity: (id: number, quantity: number) => Promise<void>;
+   addCartItem: (values: any) => Promise<void>;
+   removeCartItem: (id: number) => Promise<void>;
+}
+
+export const useCartStore = create<CartState>((set, get) => ({
+   items: [],
+   error: false,
+   loading: true,
+   totalAmount: 0,
+
+   fetchCartItems: async () => {
+      try {
+         set({ loading: true, error: false });
+         const data = await Api.cart.fetchCart();
+         set(getCartDetails(data));
+       } catch (error) {
+         console.error(error);
+         set({ error: true });
+       } finally {
+         set({ loading: false });
+       }
+   },
+
+   updateItemQuantity: async (id: number, quantity: number) => {},
+
+   addCartItem: async (values: any) => {},
+
+   removeCartItem: async (id: number) => {},
+}))
